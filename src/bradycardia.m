64a25b894168bfa35297526d855e891961dca4e4
Julien MATHET <jmathet@enseirb-matmeca.fr>
2018-05-19 19:59:58 +0200

    bradycardia + modif

---
+++ src/bradycardia.m
@@ -0,0 +1,18 @@
+function [ output_args ] = bradycardia( R_locs )
+%BRADYCARDIA Summary of this function goes here
+%% estimation du bpm
+RR = []; %vecteurs des intervalles R-R
+
+for k=1:(length(R_locs)-1)
+    RR_temp = abs(R_locs(k+1)-R_locs(k)); 
+    RR = [RR RR_temp];
+end
+bpm_estimate = (1/length(R_locs))*sum(RR);
+%% diagnostic
+res = 0; %resultat negatif
+if (bpm_estimate<60)
+    res = 1; % resultat positif
+end
+
+end
+
--- src/tachycardia.m
+++ src/tachycardia.m
@@ -1,14 +1,17 @@
 function [ res ] = tachycardia( R_locs, age )
 %TACHYCARDIA ventricular 
 RR = []; %vecteurs des intervalles R-R
+%% estimation du bpm
 for k=1:(length(R_locs)-1)
     RR_temp = abs(R_locs(k+1)-R_locs(k)); 
     RR = [RR RR_temp];
 end
 bpm_estimate = (1/length(R_locs))*sum(RR);
-res = 0;
+
+%% diagnostic
+res = 0; %resultat negatif
 if(age == 1 && bpm_estimate>160)
-    res = 1;
+    res = 1; %resultat positif
 elseif(age == 2 && bpm_estimate>130)
     res = 1;
 elseif(age>2 && age<7 && bpm_estimate>120)
@@ -19,8 +22,6 @@ elseif(age>12 && age<65 && bpm_estimate>100)
     res = 1;
 elseif(age>=65 && bpm_estimate>95)
     res = 1;
-else
-     res = 0;
 end
 
 end
