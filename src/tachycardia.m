f8e97e573cdeae638ac8d0c069d7157a3a23035d
tlepine <theo.lepine@enseirb-matmeca.fr>
2018-05-22 11:19:43 +0200

    Avancement interface (partie analyse)

--- src/tachycardia.m
+++ src/tachycardia.m
@@ -1,27 +1,43 @@
-function [ res ] = tachycardia( R_locs, age )
+function [ res ] = tachycardia( R_locs, patient_category, bpm_estimate )
 %TACHYCARDIA ventricular en fonction de l'age du patient
-RR = []; %vecteurs des intervalles R-R
-%% estimation du bpm
-for k=1:(length(R_locs)-1)
-    RR_temp = abs(R_locs(k+1)-R_locs(k)); 
-    RR = [RR RR_temp];
-end
-bpm_estimate = (1/length(R_locs))*sum(RR); %moyenne empirique sur les R-R
-%% diagnostic
-res = 0; %resultat negatif
-if(age == 1 && bpm_estimate>160)
-    res = 1; %resultat positif
-elseif(age == 2 && bpm_estimate>130)
-    res = 1;
-elseif(age>2 && age<7 && bpm_estimate>120)
-    res = 1;
-elseif(age>=7 && age<=12 && bpm_estimate>110)
-    res = 1;
-elseif(age>12 && age<65 && bpm_estimate>100)
-    res = 1;
-elseif(age>=65 && bpm_estimate>95)
-    res = 1;
-end
+% patient_category = {1 (adulte), 2 (enfant), 3 (bébé)}
+% result +-1(warning), +-2(danger) -> positif = tachy, negatif =
+% brady
 
+%% diagnostic
+res = 0; % RAS
+% Normal hearth rates are given in parenthesis, "ok" interval is centered
+% in the interval with a width of 20
+
+if (patient_category == 1) % ADULT (60-120)
+    if(bpm_estimate < 60) 
+        res=-2; % dangerous brady
+    elseif(bpm_estimate >= 60 && bpm_estimate < 80)
+        res=-1; % warning brady
+    elseif(bpm_estimate >= 100 && bpm_estimate < 120)
+        res=1; % warning tachy
+    elseif(bpm_estimate >= 120)
+        res=2; % dangerous tachy
+    end
+elseif(patient_category == 2) % CHILD (62-151)
+    if(bpm_estimate < 62) 
+        res=-2; % dangerous brady
+    elseif(bpm_estimate >= 62 && bpm_estimate < 96)
+        res=-1; % warning brady
+    elseif(bpm_estimate >= 116 && bpm_estimate < 151)
+        res=1; % warning tachy
+    elseif(bpm_estimate >= 151)
+        res=2; % dangerous tachy
+    end
+else % BABY (106-186)
+    if(bpm_estimate < 106) 
+        res=-2; % dangerous brady
+    elseif(bpm_estimate >= 106 && bpm_estimate < 136)
+        res=-1; % warning brady
+    elseif(bpm_estimate >= 156 && bpm_estimate < 186)
+        res=1; % warning tachy
+    elseif(bpm_estimate >= 186)
+        res=2; % dangerous tachy
+    end
 end
 
